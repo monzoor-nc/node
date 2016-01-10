c9c459653c32c000b0ecbd066f3e226b0a6d54ba
monzoor <monzoor@ekhanei.com>
2016-01-10 19:51:19 +0600

    set default layout

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// Default layout
+app.set("view options", { layout: "layouts/default" });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
